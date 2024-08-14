52af619f4460db4aaf233fb5f4f4cefecb658143
Arvin Albeos <arvsarvin143@gmail.com>
2024-08-14 22:05:02 +0800

    Add files via upload

---
+++ Chapter 1/TableAndChairs.java
@@ -0,0 +1,18 @@
+/** 
+   By: Arvin S. Albeos
+   Started: Aug 14, 2024 - 8:25 PM
+   Finished: Aug 14, 2024 - 8:34 PM
+   
+   Displays a pattern that shows two chairs and one table using character 'X'.
+*/
+
+public class TableAndChairs {
+   public static void main (String[] args) {
+      System.out.println("X                      X");
+      System.out.print("X                      X\n");
+      System.out.print("X      XXXXXXXXXX      X\n");
+      System.out.print("XXXXX  X        X  XXXXX\n");
+      System.out.print("X   X  X        X  X   X\n");
+      System.out.print("X   X  X        X  X   X\n");
+   }
+}
