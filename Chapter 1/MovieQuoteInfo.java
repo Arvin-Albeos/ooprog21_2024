48e2901b8237383dfa378b5b6d53fa6c3bdf07be
Arvin Albeos <arvsarvin143@gmail.com>
2024-08-14 22:07:24 +0800

    Add files via upload

---
+++ Chapter 1/MovieQuoteInfo.java
@@ -0,0 +1,17 @@
+/**
+   By: Arvin S. Albeos
+   Started: Aug 14, 2024 - 7:50 PM
+   Finished: Aug 14, 2024 - 7:55 PM
+   
+   Displays my name, my favorite movie qoute and the movie, the character that said such qoute and the year the movie was released.
+*/
+
+public class MovieQuoteInfo {
+   public static void main (String[] args) {
+      System.out.println("Student Name: Arvin S. Albeos");
+      System.out.print("\nFavorite Movie Qoute: \"You've got at least a thousand capacities in you. Even if you don't think so.\"");
+      System.out.print("\nMovie: \'All The Bright Places\'");
+      System.out.print("\nCharacter: Theodore Finch");
+      System.out.print("\nYear of the Movie: 2020\n");
+   }
+}
