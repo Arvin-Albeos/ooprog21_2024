2699a84bc044024f8a2f10d10b74b0e6f37e1dd3
Arvin Albeos <arvsarvin143@gmail.com>
2024-08-21 18:29:39 +0800

    Add files via upload

---
+++ Chapter 1/SwingAlert.java
@@ -0,0 +1,16 @@
+/*
+   By: Arvin S. Albeos
+   Started: Aug 21, 2024 - 4:07 PM
+   Finished: Aug 21, 2024 - 4:09 PM
+   
+   Displays my full name and year level and another sentence using the javax.swing.JOptionPane library. GUI Testing...
+*/
+
+import javax.swing.JOptionPane;
+
+public class SwingAlert {
+   public static void main(String[] args) {
+      JOptionPane.showMessageDialog(null, "Hello, I am Arvin S. Albeos from BSIT - 2A!");
+      JOptionPane.showMessageDialog(null, "Testing Phase using JOptionPane library.");
+   }
+}
