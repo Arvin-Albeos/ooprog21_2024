3ebb6f0e4ef0903a96069ad6f6e3bcfe92e048d7
Arvin Albeos <arvsarvin143@gmail.com>
2024-08-28 23:04:52 +0800

    Add files via upload

---
+++ Chapter 1/warmup_sleepin.java
@@ -0,0 +1,26 @@
+/* 
+   By: Arvin S. Albeos
+   Started: Aug 28, 2024 - 9:31 PM
+   Finished: Aug 28, 2024 -  PM
+   
+   The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. 
+   We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in
+*/
+
+public class warmup_sleepin {
+
+   public static boolean sleepIn(boolean weekday, boolean vacation) {
+      if (weekday != true || vacation != false) 
+         return true;
+       else 
+         return false;  
+   }
+   
+   public static void main(String[] args) {
+      System.out.println(sleepIn(false,false));
+      System.out.println(sleepIn(true,false));
+      System.out.println(sleepIn(false,true));
+   }
+   
+
+}
